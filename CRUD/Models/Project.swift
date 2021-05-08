9fb61fc336cb728da2d9a868ef0e831be4e56f88
Bambook <bambook251@gmail.com>
2021-05-08 13:17:29 +0300

    change variable

--- CRUD/Models/Project.swift
+++ CRUD/Models/Project.swift
@@ -140,7 +140,7 @@ class Kris: Object {
 
 
 class BestClass: Object {
-  @objc dynamic var bestVariable = 10
+  @objc dynamic var bestVariable = 1000
   
   func printSomething() {
     for _ in 0...bestVariable {
